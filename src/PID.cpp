23b3c54619a26b4a4ab63503d2fd90db3ee8161b
vinay <me@vnay.in>
2017-08-13 09:06:06 -0700

    first working version

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,6 +1,9 @@
 #include "PID.h"
+#include <iostream>
 
-using namespace std;
+#define _DEBUG
+
+// using namespace std;
 
 /*
 * TODO: Complete the PID class.
@@ -11,11 +14,27 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  // Assign initial K[idp] constants
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
+
+  // Initialize all errors to zero
+  this->p_error = this->i_error = this->d_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+  // Update errors:
+  d_error = cte - p_error;
+  i_error += cte;
+  p_error = cte;
+
+#ifdef _DEBUG
+  std::cout << "Kp: " << Kp << " Ki: " << Ki << " Kd: " << Kd << std::endl;
+  std::cout << "P_error: " << p_error << " I_error: " << i_error << " D_error: " << d_error << std::endl;
+#endif
 }
 
 double PID::TotalError() {
+  return -1.0 * (Kp*p_error + Ki*i_error + Kd*d_error);
 }
-
